98405d9e38df2bd9a4c8b0ff54d7d17c5be18115
Colin Farley <colinfarley0423@gmail.com>
2025-08-31 12:27:13 -0400

    put final touches on for minimal project

--- internal/pokeapi.go
+++ internal/pokeapi.go
@@ -3,15 +3,21 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
 
-func GetLocations(url string) ([]byte, error) {
+func GetRequest(url string) ([]byte, error) {
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, err	
 	}
+
+	if res.StatusCode > 299 {
+		return nil, fmt.Errorf("Unsuccessful request") 
+	}
+
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
@@ -20,10 +26,48 @@ func GetLocations(url string) ([]byte, error) {
 	return body, nil
 }
 
-func ResponseToStruct(bytes []byte, dest *LocationArea)  error {
+func ResponseToLocationArea(bytes []byte, dest *LocationArea)  error {
 	err := json.Unmarshal(bytes, &dest);
 	if err != nil {
 		return err
 	}
 	return nil
 }
+
+func ResponseToLocationInformation(bytes []byte, dest *LocationInformation)  error {
+	err := json.Unmarshal(bytes, &dest);
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ResponseToPokemon(bytes []byte, dest *Pokemon)  error {
+	err := json.Unmarshal(bytes, &dest);
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PokePrint(pokemon Pokemon) {
+	fmt.Println()
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)	
+	fmt.Println()
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats{
+		fmt.Printf(" - %s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+		
+	fmt.Println()
+	fmt.Println("Type(s):")
+	for _, typ := range pokemon.Types{
+		fmt.Printf(" - %s\n", typ.Type.Name) 
+	}
+
+	fmt.Println()
+	
+}
+
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"math/rand"
 	"fmt"
 	"os"
 	"strings"
+
 	pokeapi "github.com/colfarl/pokedexcli/internal"
 	pokecache "github.com/colfarl/pokedexcli/internal/pokecache"
 )
@@ -12,19 +14,21 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*settings) error
+	callback    func(*settings, []string) error
 }
 
 type settings struct {
 	nextLocationURL string
 	prevLocationURL string
 	cache		    *pokecache.Cache
+	pokedex			map[string]pokeapi.Pokemon
 }
 
 var config = settings{
 	nextLocationURL : "https://pokeapi.co/api/v2/location-area/",
 	prevLocationURL : "",
 	cache : pokecache.NewCache(5),
+	pokedex: make(map[string]pokeapi.Pokemon),
 }
 
 func getCommands() map[string]cliCommand {
@@ -49,6 +53,26 @@ func getCommands() map[string]cliCommand {
 			description: "explore the previous 20 locations on the map",
 			callback:    commandMapB,
 		},
+		"explore": {
+			name:        "explore",
+			description: "lists pokemon in provided area: explore <area-name>",
+			callback:    commandExplore,
+		},
+		"catch": {
+			name:        "catch",
+			description: "attempt to catch pokemon provided: catch <pokemon>",
+			callback:    commandCatch,
+		},
+		"inspect": {
+			name:        "inspect",
+			description: "get a detailed look of a pokemon you have caught: inspect <pokemon>",
+			callback:    commandInspect,
+		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "see all pokemon you have caught: pokedex",
+			callback:    commandPokedex,
+		},
 	}
 }
 
@@ -56,13 +80,13 @@ func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))	
 }
 
-func commandExit(config *settings) error {
+func commandExit(config *settings, params []string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandMap(config *settings) error {
+func commandMap(config *settings, params []string) error {
 
 	if config.nextLocationURL == "" {
 		fmt.Printf("No more locations this way!\n")
@@ -75,27 +99,29 @@ func commandMap(config *settings) error {
 	var locations pokeapi.LocationArea
 
 	if exists {
-		pokeapi.ResponseToStruct(val, &locations)
+		pokeapi.ResponseToLocationArea(val, &locations)
 	} else {
-		res, err := pokeapi.GetLocations(url)
+		res, err := pokeapi.GetRequest(url)
 		if err != nil {
 			return err
 		}
-		pokeapi.ResponseToStruct(res, &locations)
+		pokeapi.ResponseToLocationArea(res, &locations)
 		config.cache.Add(url, res)
 	}
 
 	config.nextLocationURL = locations.Next
 	config.prevLocationURL = locations.Previous
 	
+	fmt.Println()
 	for _, s := range locations.Locations {
 		fmt.Println(s.Name)
 	}
+	fmt.Println()
 
 	return nil
 }
 
-func commandMapB(config *settings) error {
+func commandMapB(config *settings, params []string) error {
 
 	if config.prevLocationURL == "" {
 		fmt.Printf("No more locations this way!\n")
@@ -108,33 +134,135 @@ func commandMapB(config *settings) error {
 	var locations pokeapi.LocationArea
 
 	if exists {
-		pokeapi.ResponseToStruct(val, &locations)
+		pokeapi.ResponseToLocationArea(val, &locations)
 	} else {
-		res, err := pokeapi.GetLocations(url)
+		res, err := pokeapi.GetRequest(url)
 		if err != nil {
 			return err
 		}
-		pokeapi.ResponseToStruct(res, &locations)
+		pokeapi.ResponseToLocationArea(res, &locations)
 		config.cache.Add(url, res)
 	}
 
 	config.nextLocationURL = locations.Next
 	config.prevLocationURL = locations.Previous
 	
+	fmt.Println()
 	for _, s := range locations.Locations {
 		fmt.Println(s.Name)
 	}
+	fmt.Println()
 
 	return nil
 }
 
-func commandHelp(config *settings) error {
+func commandHelp(config *settings, params []string) error {
 	fmt.Println("\nWelcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
 
 	for _, cmd := range getCommands() {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandExplore(config *settings, params []string) error {
+	if len(params) == 0 {
+		return fmt.Errorf("usage: explore <area-name>")
+	}
+	
+	area := params[0]
+	url := "https://pokeapi.co/api/v2/location-area/" + area
+	val, exists := config.cache.Get(url)		
+
+	var locationInfo pokeapi.LocationInformation
+
+	if exists {
+		pokeapi.ResponseToLocationInformation(val, &locationInfo)
+	} else {
+		res, err := pokeapi.GetRequest(url)
+		if err != nil {
+			return err
+		}
+		pokeapi.ResponseToLocationInformation(res, &locationInfo)
+		config.cache.Add(url, res)
+	}	
+	
+	fmt.Println()
+	for _, s := range locationInfo.PokemonEncounters {
+		fmt.Println("-", s.Pokemon.Name)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandCatch(config *settings, params []string) error {
+	if len(params) == 0 {
+		return fmt.Errorf("usage: catch <pokemon-name>")
+	}
+	
+	name := params[0]
+	url := "https://pokeapi.co/api/v2/pokemon/" + name
+	val, exists := config.cache.Get(url)
+	var pokemon pokeapi.Pokemon
+
+	if exists {
+		pokeapi.ResponseToPokemon(val, &pokemon)
+	} else {
+		res, err := pokeapi.GetRequest(url)
+		if err != nil {
+			return err
+		}
+		pokeapi.ResponseToPokemon(res, &pokemon)
+		config.cache.Add(url, res)
+	}	
+	
+	fmt.Println()
+	fmt.Printf("Throwing a Pokeball at %s...\n", name)
+	catchRoll := rand.Intn(700)	
+	
+	if(catchRoll >= pokemon.BaseExperience){
+		fmt.Printf("%s was caught!\n", name)
+		config.pokedex[name] = pokemon
+	} else {
+		fmt.Printf("%s escaped!\n", name)
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func commandInspect(config *settings, params []string) error {
+	if len(params) == 0 {
+		return fmt.Errorf("usage: inspect <pokemon-name>")
+	}	
+	
+	name := params[0]
+	pokemon, exists := config.pokedex[name]
+	if !exists {
+		fmt.Println("you have not caught that pokemon")
+	} else {
+		pokeapi.PokePrint(pokemon)
+	}
+	return nil
+}
+
+func commandPokedex(config *settings, params []string) error {
+
+	if len(params) > 0 {
+		return fmt.Errorf("usage: pokedex")
+	}	
+	
+	if len(config.pokedex) == 0 {
+		fmt.Println("you don't have any pokemon yet")
+		return nil
+	} 
+	
+	fmt.Println()
+	for key, _ := range config.pokedex {
+		fmt.Printf(" - %s\n", key)	
 	}
 	fmt.Println()
 	return nil
@@ -160,7 +288,7 @@ func main() {
 			continue
 		}
 
-		if err := cmd.callback(&config); err != nil {
+		if err := cmd.callback(&config, cleanedInput[1:]); err != nil {
 			fmt.Println(err)
 		}
 	}
